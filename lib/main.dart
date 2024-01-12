705f0d4372a1c3020f232828f41b754a147c4eea
JHHowell <jacob@alphageeks.io>
2024-01-12 13:36:18 -0600

    add keyboard controls

--- lib/main.dart
+++ lib/main.dart
@@ -77,15 +77,24 @@ class BackgroundStars extends PositionComponent {
 
 }
 
-class ShipSlammerGame extends FlameGame with KeyboardEvents {
+class ShipSlammerGame extends FlameGame with HasKeyboardHandlerComponents {
   @override
   final Vector2 canvasSize = Vector2(500, 500);
   final Vector2 viewportResolution = Vector2(500, 500);
-  late BackgroundStars bgstars;
-  late Player player;
-  late Basher basher;
+  late BackgroundStars _bgstars;
+  late Player _player;
+  late Basher _basher;
 
   late final CameraComponent cameraComponent;
+
+  static const int _speed = 200;
+  final Vector2 _direction = Vector2.zero();
+  final Map<LogicalKeyboardKey, double> _keyWeights = {
+    LogicalKeyboardKey.arrowUp: 0,
+    LogicalKeyboardKey.arrowDown: 0,
+    LogicalKeyboardKey.arrowLeft: 0,
+    LogicalKeyboardKey.arrowRight: 0,
+  };
 
   @override
   Future<void> onLoad() async {
@@ -94,23 +103,70 @@ class ShipSlammerGame extends FlameGame with KeyboardEvents {
     final world = World();
     cameraComponent = CameraComponent.withFixedResolution(width: canvasSize.x, height: canvasSize.y, world: world);
     addAll([world, cameraComponent]);
-    world.add(bgstars = BackgroundStars()
+    world.add(_bgstars = BackgroundStars()
                 ..position = Vector2(0, 0)
                 ..width = 500
                 ..height = 1500
                 ..anchor = Anchor.center);
-    world.add(basher = Basher()
+    world.add(_basher = Basher()
                 ..position = Vector2(0, -100)
                 ..height = 100
                 ..width = 100
                 ..angle = pi
                 ..anchor = Anchor.center);
-    world.add(player = Player()
+    world.add(_player = Player()
                 ..position = Vector2(0, 100)
                 ..height = 100
                 ..width = 100
                 ..angle = pi
 
                 ..anchor = Anchor.center);
+    add(
+      KeyboardListenerComponent(
+        keyUp: {
+          LogicalKeyboardKey.arrowRight: (keys) =>
+              _handleKey(LogicalKeyboardKey.arrowRight, false),
+          LogicalKeyboardKey.arrowLeft: (keys) =>
+              _handleKey(LogicalKeyboardKey.arrowLeft, false),
+          LogicalKeyboardKey.arrowDown: (keys) =>
+              _handleKey(LogicalKeyboardKey.arrowDown, false),
+          LogicalKeyboardKey.arrowUp: (keys) =>
+              _handleKey(LogicalKeyboardKey.arrowUp, false),
+        },
+        keyDown: {
+          LogicalKeyboardKey.arrowRight: (keys) =>
+              _handleKey(LogicalKeyboardKey.arrowRight, true),
+          LogicalKeyboardKey.arrowLeft: (keys) =>
+              _handleKey(LogicalKeyboardKey.arrowLeft, true),
+          LogicalKeyboardKey.arrowDown: (keys) =>
+              _handleKey(LogicalKeyboardKey.arrowDown, true),
+          LogicalKeyboardKey.arrowUp: (keys) =>
+              _handleKey(LogicalKeyboardKey.arrowUp, true),
+        },
+      ),
+    );
   }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    _direction
+      ..setValues(xInput, yInput)
+      ..normalize();
+    final displacement = _direction * (_speed * dt);
+    _player.position.sub(displacement);
+  }
+
+  bool _handleKey(LogicalKeyboardKey key, bool isDown) {
+    _keyWeights[key] = isDown ? 1 : 0;
+    return true;
+  }
+
+  double get xInput =>
+      _keyWeights[LogicalKeyboardKey.arrowLeft]! -
+      _keyWeights[LogicalKeyboardKey.arrowRight]!;
+  double get yInput =>
+      _keyWeights[LogicalKeyboardKey.arrowUp]! -
+      _keyWeights[LogicalKeyboardKey.arrowDown]!;
 }
